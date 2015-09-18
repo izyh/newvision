f745d893c1830a32bb5995c6a96b90b093d028d0
izyh <478290135@qq.com>
2015-09-18 18:44:47 +0800

    部署

--- settings.py
+++ settings.py
@@ -169,7 +169,7 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "dev.db",
         # Not used with sqlite3.
         "USER": "",
         # Not used with sqlite3.
